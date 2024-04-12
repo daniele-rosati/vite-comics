2f6df4d376df96b5e7f83e97f154d830af9af75a
daniele-rosati <drosati151@gmail.com>
2024-04-12 19:03:04 +0200

    add img cards comics

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -9,79 +9,79 @@
         data() {
 			return {
 				comics: [
-					{
-						"thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
-						"price": "$19.99",
-						"series": "Action Comics",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
-						"price": "$3.99",
-						"series": "American Vampire 1976",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
-						"price": "$16.99",
-						"series": "Aquaman",
-						"type": "graphic novel"
-					},
-					{
-						"thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
-						"price": "$2.99",
-						"series": "Batgirl",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
-						"price": "$3.99",
-						"series": "Batman",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg/revision/latest?cb=20080809201655",
-						"price": "$2.99",
-						"series": "Batman Beyond",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg/revision/latest/scale-to-width-down/1200?cb=20130627211607",
-						"price": "$3.99",
-						"series": "Batman/Superman",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg/revision/latest?cb=20200929145132",
-						"price": "$4.99",
-						"series": "Batman/Superman Annual",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg/revision/latest?cb=20200929124651",
-						"price": "$5.99",
-						"series": "Batman: The Joker War Zone",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg/revision/latest?cb=20201123054610",
-						"price": "$6.99",
-						"series": "Batman: Three Jokers",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg/revision/latest?cb=20201124094800",
-						"price": "$4.99",
-						"series": "Batman: White Knight Presents: Harley Quinn",
-						"type": "comic book"
-					},
-					{
-						"thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg/revision/latest?cb=20160303223223",
-						"price": "$16.99",
-						"series": "Catwoman",
-						"type": "graphic novel"
-					}	
-				]
+							{
+								"thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+								"price": "$19.99",
+								"series": "Action Comics",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+								"price": "$3.99",
+								"series": "American Vampire 1976",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+								"price": "$16.99",
+								"series": "Aquaman",
+								"type": "graphic novel"
+							},
+							{
+								"thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+								"price": "$2.99",
+								"series": "Batgirl",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+								"price": "$3.99",
+								"series": "Batman",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://upload.wikimedia.org/wikipedia/it/8/8d/Batman_beyond_1-195x300.jpg",
+								"price": "$2.99",
+								"series": "Batman Beyond",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://www.fumetto-online.it/ew/ew_albi/images/DC%20COMICS/323412_20141217060820_large.jpg",
+								"price": "$3.99",
+								"series": "Batman/Superman",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/09/Batman-Superman-Annual-Cv1.jpg",
+								"price": "$4.99",
+								"series": "Batman/Superman Annual",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://m.media-amazon.com/images/I/41nGyVF7C2L.jpg",
+								"price": "$5.99",
+								"series": "Batman: The Joker War Zone",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://www.ibs.it/images/9788828737711_0_536_0_75.jpg",
+								"price": "$6.99",
+								"series": "Batman: Three Jokers",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://i.ebayimg.com/images/g/YgYAAOSwu0BfjPo5/s-l1200.jpg",
+								"price": "$4.99",
+								"series": "Batman: White Knight Presents: Harley Quinn",
+								"type": "comic book"
+							},
+							{
+								"thumb": "https://mycomics.it/wp-content/uploads/sites/16/2022/12/FUMCAWOPA0008.jpg",
+								"price": "$16.99",
+								"series": "Catwoman",
+								"type": "graphic novel"
+							}
+							]
 			}
 		},
     };
@@ -102,18 +102,18 @@
 </template>
 
 <style scoped lang="scss">
-    // @use "./partials/_variables.scss" as *;
+    @use '../style/partials/variables' as *;
 
     section.hero{
         height: 300px;
-        background-image: url(../assets/img/jumbotron.jpg);
+        background-image: url(../src/assets/img/jumbotron.jpg);
         background-position: center top;
         background-size: cover;
         .container{
             height: 100%;
             position: relative;
             div{
-                // background-color: $primary_color;
+                background-color: $primary_color;
                 text-transform: uppercase;
                 padding: 10px 20px;
                 color: white;
@@ -127,7 +127,7 @@
     }
 
     section.comics{
-        // background-color: $secondary_dark;
+        background-color: $secondary_dark;
         color: white;
         .container{
             padding: 20px 0px;
@@ -136,7 +136,7 @@
             justify-content: space-between;
         }
         button{
-            // background-color: $primary_color;
+            background-color: $primary_color;
             text-transform: uppercase;
             padding: 10px 40px;
             color: white;
