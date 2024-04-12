c9d865a58bf7076bd910a92c1e44a86109dbbc19
daniele-rosati <drosati151@gmail.com>
2024-04-12 17:27:24 +0200

    App vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import AppHeader from "./components/AppHeader.vue"
+	import AppCards from "./components/AppCards.vue"
+
+	import AppContacts from "./components/AppContacts.vue"
+
+	export default {
+		components: {
+			AppHeader,
+			AppCards,
+			AppContacts,
+		},
+	}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<AppHeader></AppHeader>
+	<main>
+		<AppCards></AppCards>
+		
+	</main>
+	<footer>
+	
+		<AppContacts></AppContacts>
+	</footer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+	@use "./style/generic";
 </style>
